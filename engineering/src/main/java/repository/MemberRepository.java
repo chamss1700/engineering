48f2124c8ee0f85cc6f4cd7d437194d578bb0b62
jungdaeyong <jungdaeyong@DESKTOP-F5L9UAN>
2021-06-15 22:03:49 +0900

    2021-06-15 수업

--- engineering/src/main/java/repository/MemberRepository.java
+++ engineering/src/main/java/repository/MemberRepository.java
@@ -12,6 +12,22 @@ public class MemberRepository {
 	SqlSession sqlSession;
 	private final String namesace="";
 	private String statement;
+	
+	public void memDel(String memId) {
+		statement = namesace + ".memberDelete";
+		sqlSession.selectOne(statement, memId);
+	}
+	
+	public MemberDTO memInfo(String memId) {
+		statement = namesace + ".memInfo";
+		return sqlSession.selectOne(statement, memId);
+	}
+	
+	public void memUpdate(MemberDTO dto) {
+		statement = namesace + ".memUpdate";
+		sqlSession.update(statement, dto);		
+	}
+	
 	public List<MemberDTO> memList() {
 		statement = namesace + ".memberList";
 		return sqlSession.selectList(statement);
